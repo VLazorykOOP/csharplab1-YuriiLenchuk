37ec2893df98436f5a0800a7504d344e9879ddbf
YuriiLenchuk <lenchuk.yurii@chnu.edu.ua>
2024-02-09 15:12:58 +0200

    task1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,16 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
- static void task1()
+﻿using System;
+static void task1()
 {
-    Console.WriteLine("Task1 !");
-    Console.Write("s= ");
-    string? str = Console.ReadLine();
-    float s = 0; 
-    if (str != null) s = float.Parse(str);
-    double p = 4 * Math.Sqrt(s);
-    Console.WriteLine("p=" + p);
-
+    Console.WriteLine("Enter two numers: ");
+    int x, y;
+    x = int.Parse(Console.ReadLine());
+    y = int.Parse(Console.ReadLine());
+    Console.WriteLine("Mid Geometrical: " + Math.Sqrt(Math.Abs(x) * Math.Abs(y)));
 }
-Console.WriteLine("Lab 1 !");
 task1();
-// continue ...
